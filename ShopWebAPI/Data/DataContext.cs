0637c277a6d6959eaaff060c2971c825023790ad
Alexander <50077233+Sekir0@users.noreply.github.com>
2020-03-19 18:45:02 +0200

    Add DbContext

---
+++ ShopWebAPI/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShopWebAPI.DAL.Models;
+
+namespace ShopWebAPI.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
