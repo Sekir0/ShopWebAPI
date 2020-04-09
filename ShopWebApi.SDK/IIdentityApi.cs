a7e9b0175b03d41192adb4b3735d4af4d003af61
Alexander <50077233+Sekir0@users.noreply.github.com>
2020-04-09 13:44:57 +0100

    Aded SDK and SDK Sample

---
+++ ShopWebApi.SDK/IIdentityApi.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Refit;
+using ShopWebAPI.Contracts.V1.Requests;
+using ShopWebAPI.Contracts.V1.Responses;
+
+namespace ShopWebApi.SDK
+{
+    public interface IIdentityApi
+    {
+        [Post("/api/v1/identity/register")]
+        Task<ApiResponse<AuthSuccessResponse>> RegisterAsynk([Body] UserRegistrationRequest registrationRequest);
+
+        [Post("/api/v1/identity/login")]
+        Task<ApiResponse<AuthSuccessResponse>> LoginAsynk([Body] UserLoginRequest loginRequest);
+
+        [Post("/api/v1/identity/refresh")]
+        Task<ApiResponse<AuthSuccessResponse>> TokenRefreshAsynk([Body] RefreshTokenRequest refreshRequest);
+    }
+}
---
+++ ShopWebApi.SDK/IShopWebApi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Refit;
+using ShopWebAPI.Contracts.V1.Requests;
+using ShopWebAPI.Contracts.V1.Responses;
+
+namespace ShopWebApi.SDK
+{
+    [Headers("Authorization: Bearer")]
+    public interface IShopWebApi
+    {
+        [Get("/api/v1/products")]
+        Task<ApiResponse<List<ProductResponse>>> GetAllAsynk();
+
+        [Get("/api/v1/products/{productId}")]
+        Task<ApiResponse<ProductResponse>> GetAsynk(Guid productId);
+
+        [Post("/api/vq/products")]
+        Task<ApiResponse<ProductResponse>> CreateAsynk([Body] CreateProductRequest createProductRequest);
+
+        [Post("/api/vq/products/{productId}")]
+        Task<ApiResponse<ProductResponse>> UpdateAsynk(Guid productId, [Body] UpdateProductRequest createProductRequest);
+
+        [Post("/api/vq/products/{productId}")]
+        Task<ApiResponse<ProductResponse>> DeleteAsynk(Guid productId);
+    }
+}
